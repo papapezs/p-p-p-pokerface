eb929c84bab473e3efce23af722be72766fc2f2b
Ana Azevedo <ana.azevedo@medicore.nl>
2018-09-14 11:38:20 +0200

    Add exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,54 @@
 (ns p-p-p-pokerface)
 
+(def face-rank {\T 10, \J 11, \Q 12, \K 13, \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[r] card]
+    (if (Character/isDigit r) (Integer/valueOf (str r)) (Integer/valueOf (face-rank r)))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (= (apply max (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= (apply max (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= (apply max (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (= (apply max (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (sort (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freq (frequencies (vals (frequencies (map rank hand))))
+        find-freq (fn [f] (find freq f))
+        has-freq (fn [f many-times] (if (empty? (find-freq f)) false (= many-times ((find-freq f) 1))))]
+    (or (has-freq 2 2) (has-freq 4 1))))
 
 (defn straight? [hand]
-  nil)
+  (let [get-rank-hand (fn [x] (sort (map rank x)))
+        get-rank-low-ace-hand (fn [x] (sort (replace {14 1} (map rank x))))
+        get-min-rank (fn [x] (apply min x))
+        get-range (fn [x] (range (get-min-rank x) (+ (get-min-rank x) 5)))]
+    (or (= (get-rank-hand hand) (get-range (get-rank-hand hand))) (= (get-rank-low-ace-hand hand) (get-range (get-rank-low-ace-hand hand))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                   [two-pairs? 2]  [three-of-a-kind? 3]
+                   [straight? 4]   [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}
+        call-f (fn [f] (if ((f 0) hand) (f 1)))]
+    (apply max (filter some? (map call-f checkers)))))
